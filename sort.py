1bc2e205fcb11cf163751963b6957fd3bf1a6442
Sargis <sargishunanyan02@gmail.com>
2023-06-01 18:18:47 +0400

    added  insertion

--- sort.py
+++ sort.py
@@ -42,14 +42,19 @@ def bubble(arr):
                 arr[i],arr[i+1]=arr[i+1],arr[i]
     return arr
 
-
-
-
-
-
-
+def insertion(arr):
+    n=len(arr)
+    for i in range(n-1):
+        if arr[i]>arr[i+1]:
+            for j in range(i+1,-1,-1):
+                arr[j], arr[j-1]=arr[j-1],arr[j]
+                if arr[j-1]<=arr[j]:
+                    break
+    return arr
+            
 arr=[2,3,2,5,1,6,4,20,5]
 print(merge(arr))
 print(selection(arr))
 print(bubble(arr))
+print(insertion(arr))
 
