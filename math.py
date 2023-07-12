6f8664d9683bfda4880a0c77e5b3391292a55c3f
Sargis <sargishunanyan02@gmail.com>
2023-07-12 17:07:05 +0400

    Gaussian dist

--- math.py
+++ math.py
@@ -1,4 +1,4 @@
-#GrayCode, 
+#GrayCode, Gaussian distribution(properties)
 ##################
 
 # bits=4
@@ -14,5 +14,30 @@
 #     lst=list(map(lambda x: '0'+x, lst))+list(map(lambda x: '1'+x, lst[-1::-1]))
 # print('After Gray Code algorithm. This is the Hamiltonian loop.')
 # print(*lst, sep='\n')
+
+#####################
+
+x = [12, 15, 25, 20, 18]
+y = [31, 21, 25, 30, 40]
+
+
+def E(a): #Expectation
+    return sum(a)/len(a)
+
+def D(a): #Dispersion, SD^2
+    a2=list(map(lambda x:x**2, a))
+    return E(a2)-E(a)**2
+
+def cov(a,b):
+    n=len(a)
+    summa=0
+    for i in range(n):
+        summa+=(a[i]-E(a))*(b[i]-E(b))
+    return summa/n
+
+def corr(a,b):
+    return cov(a,b)/(D(a)*D(b))**0.5
+
+print(corr(x,y), cov(x,y), E(x), E(y), D(x), D(y))
 
 #####################
